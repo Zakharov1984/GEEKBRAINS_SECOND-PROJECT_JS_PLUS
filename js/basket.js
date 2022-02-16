855a38244d3a251f19102ad58229229e57bc11b0
Zakharov <zakharov.products@gmail.com>
2022-02-16 06:15:38 +0300

    added_product_rendering_in_basket

--- js/basket.js
+++ js/basket.js
@@ -1,16 +1,31 @@
 let basketItems = [];
+let tableElement = document.querySelector('.table-items');
 
 document.querySelector('.basket').addEventListener('click', event => {
     document.querySelector('.basket__box').classList.toggle('basket__box_hidden');
 })
 
 function AddToCartButtons() {
-    const buttons = document.querySelectorAll('.item__added');
-    for (let button of buttons) {
-        button.addEventListener('click', event => {
-            event.target.dataset
+    const itemElements = document.querySelectorAll('.item');
+    for (let item of itemElements) {
+        item.addEventListener('click', event => {
+            MarkupItemInBasket(products[event.currentTarget.id]);
         })
     }
 }
 
 AddToCartButtons();
+
+
+function MarkupItemInBasket(itemObject) {
+    let MarkupItem =    `
+                        <tr class="table-items__item">
+                            <td>${itemObject.name}</td>
+                            <td>${itemObject.counter}</td>
+                            <td>${itemObject.price}</td>
+                            <td>${itemObject.counter * itemObject.price}</td>
+                        </tr>
+                        `
+    tableElement.insertAdjacentHTML("beforeend", MarkupItem);
+   
+}
